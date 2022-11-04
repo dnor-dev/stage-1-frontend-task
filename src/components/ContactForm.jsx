7d5f257c311ab51a4a36d81d237ff1e104c71155
dnor-dev <anorue4rss@hotmail.com>
2022-11-04 15:23:38 +0100

    chore: code checks

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { Helmet } from 'react-helmet'
 
 const ContactForm = () => {
   const handleChange = e => {
@@ -12,68 +13,73 @@ const ContactForm = () => {
   const [error, setError] = useState(false)
 
   return (
-    <div className="contact-form">
-      <h1>Contact Me</h1>
-      <p>Hi there, contact me to ask anything you have in mind</p>
+    <>
+      <Helmet>
+        <title>Contact DNor</title>
+      </Helmet>
+      <div className="contact-form">
+        <h1>Contact Me</h1>
+        <p>Hi there, contact me to ask anything you have in mind</p>
 
-      <form action="" className="form">
-        <div className="flex-names">
+        <form action="" className="form">
+          <div className="flex-names">
+            <div>
+              <label htmlFor="first_name">First name</label>
+              <input
+                type="text"
+                placeholder="Enter your firstname"
+                id="first_name"
+                required
+              />
+            </div>
+            <div>
+              <label htmlFor="last_name">Last name</label>
+              <input
+                type="text"
+                placeholder="Enter your lastname"
+                id="last_name"
+                required
+              />
+            </div>
+          </div>
           <div>
-            <label htmlFor="first_name">First name</label>
+            <label htmlFor="email">Email</label>
             <input
-              type="text"
-              placeholder="Enter your firstname"
-              id="first_name"
+              type="email"
+              placeholder="yourname@email.com"
+              id="email"
               required
             />
           </div>
           <div>
-            <label htmlFor="last_name">Last name</label>
-            <input
-              type="text"
-              placeholder="Enter your lastname"
-              id="last_name"
-              required
-            />
+            <label htmlFor="message">Message</label>
+            <textarea
+              name="message"
+              id="message"
+              placeholder="Send me a message and i'll reply you as soon as possible..."
+              onChange={handleChange}
+              className={error ? 'inputError' : undefined}
+            ></textarea>
+            {error && <p className="error-text">Please enter a message</p>}
           </div>
-        </div>
-        <div>
-          <label htmlFor="email">Email</label>
+          <div>
+            <label htmlFor="agree" className="checkbox-container">
+              You agree to providing your contact to DNor who may contact you.
+              <input type="checkbox" id="agree" />
+              <span className="checkmark"></span>
+            </label>
+          </div>
+
           <input
-            type="email"
-            placeholder="yourname@email.com"
-            id="email"
-            required
+            type="submit"
+            value="Send Message"
+            className="submit-btn"
+            id="btn__submit"
+            disabled={error}
           />
-        </div>
-        <div>
-          <label htmlFor="message">Message</label>
-          <textarea
-            name="message"
-            id="message"
-            placeholder="Send me a message and i'll reply you as soon as possible..."
-            onChange={handleChange}
-            className={error ? 'inputError' : undefined}
-          ></textarea>
-          {error && <p className="error-text">Please enter a message</p>}
-        </div>
-        <div>
-          <label htmlFor="agree" className="checkbox-container">
-            You agree to providing your contact to name who may contact you.
-            <input type="checkbox" id="agree" />
-            <span className="checkmark"></span>
-          </label>
-        </div>
-
-        <input
-          type="submit"
-          value="Send Message"
-          className="submit-btn"
-          id="btn__submit"
-          disabled={error}
-        />
-      </form>
-    </div>
+        </form>
+      </div>
+    </>
   )
 }
 
