67cf3506fe362e6be052c0f380129f4028aec628
dnor-dev <anorue4rss@hotmail.com>
2022-11-07 10:07:02 +0100

    bug: removed animation

--- src/router.js
+++ src/router.js
@@ -1,27 +1,15 @@
-import { lazy, Suspense } from 'react'
+import { lazy } from 'react'
 import { Route, Routes } from 'react-router-dom'
-import { AnimatePresence, motion } from 'framer-motion'
-import Loader from './components/Loader'
 
 const Home = lazy(() => import('./components/App'))
 const Form = lazy(() => import('./components/ContactForm'))
 
 const AppRoutes = () => {
   return (
-    <AnimatePresence>
-      <Suspense fallback={<Loader />}>
-        <motion.div
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          exit={{ opacity: 0 }}
-        >
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/contact" element={<Form />} />
-          </Routes>
-        </motion.div>
-      </Suspense>
-    </AnimatePresence>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/contact" element={<Form />} />
+    </Routes>
   )
 }
 
